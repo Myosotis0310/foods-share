50979af559ca70903e0c4f80dd5b08946512a73f
Joey <myosotis0310@gmail.com>
2024-10-08 19:54:11 +0800

    创建实体类和相应mapper接口

---
+++ springboot/src/main/java/com/group2/foodsshare/entity/Category.java
@@ -0,0 +1,35 @@
+package com.group2.foodsshare.entity;
+
+/**
+ * 菜谱类别
+ */
+public class Category {
+
+    private Integer id;
+    private String name;
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ springboot/src/main/java/com/group2/foodsshare/mapper/LikesMapper.java
@@ -0,0 +1,18 @@
+package com.group2.foodsshare.mapper;
+
+import com.group2.foodsshare.entity.Likes;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+
+import java.util.List;
+
+public interface LikesMapper {
+
+    List<Likes> selectLikes(Likes likes);
+
+    @Delete("delete from likes where fid = #{fid} and user_id = #{userId} and module = #{module}")
+    void delete(Likes likes);
+
+    @Insert("insert into likes (fid, user_id, module) values(#{fid}, #{userId}, #{module})")
+    void insert(Likes likes);
+}
